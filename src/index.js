c42f8abb1c0378bf641526af5eb6842fd383ce19
Caio <caiopadilha@id.uff.br>
2022-03-04 19:20:23 -0300

    Validações de email e senha funcionando, botão habilitando

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Login from './pages/Login';
-import Wallet from './pages/Wallet';
+
 import store from './store';
 
 ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
       <App />
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/carteira" component={ Wallet } />
-      </Switch>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,15 +4,65 @@ import PropTypes from 'prop-types';
 import { userLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      botaoLoginDesabilitado: true,
+    };
+  }
+
+  handleInputChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.validarBotao);
+  }
+  // Tenho que chamar a validarbotao aqui toda vez para a cada alteração, ver se habilita o botão ou não.
+
+  validarBotao = () => {
+    const { email, password } = this.state;
+    const MINIMO_SEIS = 6;
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const emailValido = emailRegex.test(email);
+
+    if (password.length >= MINIMO_SEIS && emailValido) {
+      this.setState({ botaoLoginDesabilitado: false });
+    } else {
+      this.setState({ botaoLoginDesabilitado: true });
+    }
+  }
+
+  clickBotaoLogin = () => {
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+
+    dispatch(userLogin(email));
+    history.push('/carteira');
+
+  }
+
   render() {
+    const { botaoLoginDesabilitado, email, password } = this.state;
     return (
       <div>
-        <input type="email" data-testid="email-input" />
-        <input type="password" data-testid="password-input" />
-        <button type="submit">Entrar</button>
+        <input
+          type="email"
+          data-testid="email-input"
+          name="email"
+          value={ email }
+          onChange={ this.handleInputChange }
+        />
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ this.handleInputChange }
+        />
+        <button type="button" disabled={ botaoLoginDesabilitado }>Entrar</button>
       </div>
     );
   }
 }
 
-export default Login;
+export default connect()(Login);
