b2c1a30a6a6b4315d3d3d9ef52018626924ba1dc
Caio <caiopadilha@id.uff.br>
2022-03-07 15:00:10 -0300

    Componentizando

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,50 +1,17 @@
 import React from 'react';
 import Header from '../components/Header';
+import InputDespesas from '../components/InputDespesas';
+import TabelaDespesas from '../components/TabelaDespesas';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div>
         <Header />
-        Valor:
-        <input data-testid="value-input" type="number" />
-        Descrição:
-        <input data-testid="description-input" type="text" />
-        {/* <label htmlFor="currency-input">
-          <select data-testid="currency-input" name="currency-input" />
-        </label> */}
-        Método de pagamento:
-        <select data-testid="method-input">
-          <option value="Dinheiro">Dinheiro</option>
-          <option value="Cartão de crédito">Cartão de crédito</option>
-          <option value="Cartão de débito">Cartão de débito</option>
-        </select>
-        Tag:
-        <select data-testid="tag-input">
-          <option value="Alimentação">Alimentação</option>
-          <option value="Lazer">Lazer</option>
-          <option value="Trabalho">Trabalho</option>
-          <option value="Transporte">Transporte</option>
-          <option value="Saúde">Saúde</option>
-        </select>
+        <InputDespesas />
         <hr />
-        <table>
-          <thead>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
-            </tr>
-          </thead>
-        </table>
+        <TabelaDespesas />
       </div>
-
     );
   }
 }
