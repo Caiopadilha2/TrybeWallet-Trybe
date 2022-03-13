b1dc64304340419b029a6a5e40821a7fa5ce3b2d
Caio <caiopadilha@id.uff.br>
2022-03-13 15:47:57 -0300

    Req 07

--- src/components/TabelaDespesas.jsx
+++ src/components/TabelaDespesas.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class TabelaDespesas extends React.Component {
   render() {
+    const { wallet: { expenses } } = this.props;
+
     return (
       <div>
         {/* https://www.w3schools.com/tags/tag_th.asp */}
@@ -19,10 +23,40 @@ class TabelaDespesas extends React.Component {
               <th>Editar/Excluir</th>
             </tr>
           </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={ expense.id }>
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ Number(expense.value).toFixed(2) }</td>
+                <td>{expense.exchangeRates[expense.currency].name}</td>
+                <td>{Number(expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+                <td>
+                  { (Number(expense.value).toFixed(2)
+                   * Number(expense.exchangeRates[expense.currency].ask))}
+                  {' '}
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
 
-export default TabelaDespesas;
+const mapStateToProps = (state) => ({
+  wallet: state.wallet,
+});
+
+TabelaDespesas.propTypes = {
+  wallet: PropTypes.shape({
+    expenses: PropTypes.arrayOf(PropTypes.object),
+  }),
+}.isRequired;
+
+// https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html#proptypes
+
+export default connect(mapStateToProps)(TabelaDespesas);
